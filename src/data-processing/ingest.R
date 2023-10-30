36a7fd8624133dae8fe57af9320a3c5846cc7ab7
Wouter van Atteveldt <wouter@vanatteveldt.com>
2023-10-30 14:57:24 +0100

    Add topics, updated meta

--- src/data-processing/ingest.R
+++ src/data-processing/ingest.R
@@ -5,22 +5,28 @@ library(tidyverse)
 
 d_raw <- readtext(here("data/raw/*.pdf"))
 # d |> select(title) |> unique() |> write_csv(here("data/raw/meta.csv"))
-
+meta <- read_csv(here("data/raw/meta.csv")) |> rename(file=title)
 
 d <- d_raw |> 
   unnest_tokens(paragraph, text, token="paragraphs", to_lower=FALSE) |> 
   mutate(paragraph=str_replace_all(paragraph, "\\s+", " ") |> trimws())|> 
   group_by(doc_id, paragraph) |> filter(n() <= 2) |> ungroup() |>
   filter(str_detect(paragraph, "[A-Za-z]")) |> 
-  select(title=doc_id, text=paragraph) |> 
+  select(file=doc_id, text=paragraph) |> 
   add_column(date="2023-10-01") |>
-  inner_join(meta)
+  inner_join(meta) |>
+  mutate(title=str_c(party, " partijprogramma 2023")) |>
+  select(-file)
 
 
 View(d)
 
 library(amcat4r)
-amcat4r::amcat_login("https://amcat4.labs.vu.nl/amcat")
+#amcat4r::amcat_login("https://amcat4.labs.vu.nl/amcat")
+#amcat4r::delete_index("2023-nl-manifestos")
+
+
+amcat4r::amcat_login("https://open.amcat.nl/amcat")
 amcat4r::delete_index("2023-nl-manifestos")
 amcat4r::create_index("2023-nl-manifestos", name="2023 Dutch Manifestos")
 amcat4r::set_fields("2023-nl-manifestos", list(party="keyword"))
--- src/data-processing/topics.py
+++ src/data-processing/topics.py
@@ -1,6 +1,75 @@
-# Use a pipeline as a high-level helper
-from transformers import pipeline
+import collections
+import logging
+import torch
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import csv
+from amcat4py import AmcatClient
 
-pipe = pipeline("text-classification", model="manifesto-project/manifestoberta-xlm-roberta-56policy-topics-context-2023-1-1", trust_remote_code=True)
 
-print(pipe("Bestaanszekerheid is het belangrijkste onderwerp van 2023"))
+class CMPClassifier:
+    def __init__(self):
+        logging.info("Loading CMP topics...")
+        self.topics = {
+            int(row["cmp"]): row["label"]
+            for row in csv.DictReader(open("data/raw/cmp_topics.csv"))
+        }
+        logging.info("Loading CMP BERT model...")
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            "manifesto-project/manifestoberta-xlm-roberta-56policy-topics-context-2023-1-1",
+            trust_remote_code=True,
+        )
+        logging.info("Loading CMP Tokenizer...")
+        self.tokenizer = AutoTokenizer.from_pretrained("xlm-roberta-large")
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        torch.cuda.empty_cache()
+        logging.info("Using device {self.device}")
+        self.model.to(self.device)
+
+    def predict(self, text, context=None):
+        inputs = self.tokenizer(
+            text,
+            context or text,
+            return_tensors="pt",
+            max_length=300,  # we limited the input to 300 tokens during finetuning
+            padding="max_length",
+            truncation=True,
+        ).to(self.device)
+
+        logits = self.model(**inputs).logits
+        probabilities = torch.softmax(logits, dim=1).tolist()[0]
+
+        for i, p in sorted(enumerate(probabilities), key=lambda item: -item[1]):
+            yield self.model.config.id2label[i], p
+
+    def predict_vu(self, text, context=None):
+        result = collections.defaultdict(int)
+        for c, p in self.predict(text, context):
+            code = int(c.split(" ")[0])
+            label = self.topics[code]
+            result[label] += p
+        return sorted(result.items(), key=lambda item: -item[1])
+
+
+logging.basicConfig(
+    format="[%(asctime)s %(levelname)-7s] %(message)s", level=logging.INFO
+)
+
+classifier = CMPClassifier()
+
+HOST = "https://open.amcat.nl/amcat"
+INDEX = "2023-nl-manifestos"
+
+logging.info(f"Logging into AmCAT at {HOST}")
+amcat = AmcatClient(HOST)
+amcat.login()
+amcat.set_fields(INDEX, dict(cmp="tag", issue="tag", issue_confidence="double"))
+
+logging.info("Querying documents")
+for i, doc in enumerate(amcat.query(INDEX, fields=["text"])):
+    if not i % 100:
+        logging.info(i)
+    cmp, confidence = list(classifier.predict(doc["text"]))[0]
+    cmp_code = int(cmp.split(" ")[0])
+    vu = classifier.topics[cmp_code]
+    fields = dict(cmp=cmp, issue=vu, issue_confidence=confidence)
+    amcat.update_document(INDEX, doc_id=doc["_id"], body=fields)
